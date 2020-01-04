265879f6800624a4814ed2a60962f0dbcd055dc1
psprawka <spraweczga@o2.pl>
2020-01-04 23:12:53 +0100

    more tests done, verbose mode, file and console input mode

--- computor_v1.py
+++ computor_v1.py
@@ -15,6 +15,7 @@ class settings:
 	MAROON = "\033[38;5;88m"
 	GREY = '\033[38;5;246m'
 	verbose = False
+	allowed_char_set = "X+-*/1234567890^ "
 
 # ---------------------------------------- MATHS ----------------------------------------
 def square_root(number):
@@ -45,15 +46,25 @@ def get_coeff(equation):
 
 def parse_equation(equation):
 	left_right = equation.split("=")
-	print_info(20, True, obj=left_right)
+	print_info(-1, obj1=left_right[0].strip(), obj2=left_right[1].strip())
 	left_right = [get_coeff(left_right[0]), get_coeff(left_right[1])]
-	print_info(21, True, obj=left_right)
+	print_info(-2, obj1=left_right[0], obj2=left_right[1])
 	longer, shorter = (left_right[0], left_right[1]) if len(left_right[0]) >= len(left_right[1]) else (left_right[1], left_right[0])
 	for i in range(len(shorter)):
 		longer[i] -= shorter[i]
-	print_info(22, True, obj=longer)
+	for x in longer[1:][::-1]:
+		if x != 0:
+			break
+		longer = longer[0:-1]
+	print_info(-3, obj1=longer)
 	return longer
 
+def valid_equation(equation):
+	for x in equation:
+		if x not in settings.allowed_char_set:
+			print_info(12, obj1=x, obj2=equation)
+			print_info(1)
+			exit(0)
 
 # -------------------------------------- PRINTING --------------------------------------
 def concat_reduced(coeff):
@@ -68,57 +79,62 @@ def concat_reduced(coeff):
 	res.append("= 0")
 	return "".join(res)
 
-def print_info(argument, is_verbose, x=0, y=0, obj=[0,0]):
+def print_info(argument, x=0, y=0, obj1=[0], obj2=[0]):
 	prints = {
-		1: "Usage: python3 computor_v1.py [-v | --verbose | -f filename | --file filename] <equation[s]>",
+		1: "Usage: python3 computor_v1.py [-v | --verbose] [-f filename | --file filename] <equation[s]>",
 		2: "The polynomial degree is stricly greater than 2, I can't solve.",
-		3: "Discriminant is strictly positive, the two solutions are:\n%f\n%f" % (x, y),
-		4: "Discriminant is zero, one solution:\n%f" % x,
+		3: "Discriminant is strictly positive, the two solutions are:\nx = %g or x = %g" % (x, y),
+		4: "Discriminant is zero, one solution: x = %g" % x,
 		5: "Discriminant is less than 0, no solution.",
-		6: "The solution is: %f" % x,
+		6: "The solution is: x = %g" % x,
 		7: "Identity - all of real numbers are the solution.",
 		8: "Contradiction - none of real nuumers is the solution.",
-		9: "Equation: %s" % obj,
-		10: "Reduced:  %s" % obj,
+		9: "Equation: %s" % obj1,
+		10: "Reduced:  %s" % obj1,
 		11: "Polynomial degree: %d" % x,
-		20: "Left side of equation: [%s] | Right side of equation: [%s]" % (obj[0], obj[1]),
-		21: "Left coefficients: %s | Right coefficients: %s" % (obj[0], obj[1]),
-		22: "Reduced coefficients: %s" % obj,
-		23: "Discriminant = b^2 - 4ac = %d" % x
+		12: "Error: Invalid character '%s' in equation '%s'" % (obj1, obj2),
+		-1: "Left side of equation: [%s] | Right side of equation: [%s]" % (obj1, obj2),
+		-2: "Left coefficients: %s | Right coefficients: %s" % (obj1, obj2),
+		-3: "Reduced coefficients: %s" % obj1,
+		-4: "Discriminant = b^2 - 4ac = %d" % x
 	}
-	if is_verbose and settings.verbose:
-		print(settings.CYAN, end="")
-	print(prints.get(argument), settings.NORMAL)
+	if argument > 0:
+		print(prints.get(argument))
+	elif settings.verbose:
+		print(settings.CYAN, prints.get(argument), settings.NORMAL, sep="")
+
 
 # ---------------------------------- HANDLE EQUATIONS ----------------------------------
+def handle_equation(equation):
+	valid_equation(equation)
+	print("--------------------------------------------------")
+	print_info(9, obj1=equation)
+	coefficients = parse_equation(equation)
+	print_info(10, obj1=concat_reduced(coefficients))
+	print_info(11, len(coefficients) - 1)
+	compute_result(coefficients)
+
 def handle_args_equations():
 	for i in range(1 if not settings.verbose else 2, len(sys.argv)):
-		try:
-			print("\n--------------------ARGS------------------------------")
-			print_info(9, False, obj=str(sys.argv[i]))
-			coefficients = parse_equation(str(sys.argv[i]))
-			print_info(10, False, obj=concat_reduced(coefficients))
-			print_info(11, False, len(coefficients) - 1)
-			compute_result(coefficients)
-			print("---------------------------------------------------")
-		except:
-			print_info(1, False)
-			return 1
+		# try:
+		handle_equation(str(sys.argv[i]))
+		# except:
+		# 	print_info(1)
+		# 	return 1
 
 def handle_file_equations(filename):
 	with open(filename) as file:
+		
 		for cnt, line in enumerate(file):
+			
 			try:
-				line = line.strip()
-				print("\n----------------------FILE-----------------------------")
-				print_info(9, False, obj=line)
-				coefficients = parse_equation(line)
-				print_info(10, False, obj=concat_reduced(coefficients))
-				print_info(11, False, len(coefficients) - 1)
-				compute_result(coefficients)
-				print("---------------------------------------------------")
+				if line and '#' in line:
+					line = line.split("#")[0]
+				if len(line.strip()) > 0:
+					handle_equation(line.strip())
+					pass
 			except:
-				print_info(1, False)
+				print_info(1)
 				return 1
 
 
@@ -126,28 +142,41 @@ def handle_file_equations(filename):
 def compute_result(coeff):
 	len_coeff = len(coeff)
 	if len_coeff > 3:
-		print_info(2, False)
+		print_info(2)
 	elif len_coeff == 3:
 		c, b, a = coeff[0], coeff[1], coeff[2]
 		delta = (b ** 2) - (4 * a * c)
-		print_info(23, True, delta)
+		print_info(-4, delta)
 		if delta > 0:
 			s1, s2 = (-b-square_root(delta)) / (2*a), (-b+square_root(delta)) / (2*a)
-			print_info(3, False, s1, s2)
+			print_info(3, s1, s2)
 		elif delta == 0:
-			print_info(4, False, (-b) / (2*a))
-		else:
-			print_info(5, False)
+			print_info(4, (-b) / (2*a))
+		else: 
+			print_info(5)
 	elif len_coeff == 2:
-		print_info(6, False, 0 if coeff[1] == 0 else -(coeff[0]/coeff[1]))
+		print_info(6, 0 if coeff[1] == 0 else -(coeff[0]/coeff[1]))
 	elif len_coeff == 1:
-		print_info(7 if coeff[0] == 0 else 8, False)
+		print_info(7 if coeff[0] == 0 else 8)
 
 def main():
-	if len(sys.argv) < 2 or (len(sys.argv) == 2 and str(sys.argv) == "-v"):
-		print_info(1, False)
-		exit(0)
-
+	if len(sys.argv) < 2:
+		inp = input("Insert equation here: ")
+		try:
+			handle_equation(inp)
+		except:
+			print_info(1)
+		return 0
+	
+	if len(sys.argv) == 2 and (str(sys.argv[1]) == "-v" or str(sys.argv[1]) == "--verbose"):
+		settings.verbose = True
+		inp = input("Insert equation here: ")
+		try:
+			handle_equation(inp)
+		except:
+			print_info(1)
+		return 0
+	
 	for i in range(1, len(sys.argv)):
 		if str(sys.argv[i]) == "-v" or str(sys.argv[i]) == "--verbose":
 			settings.verbose = True
@@ -157,9 +186,8 @@ def main():
 				handle_file_equations(str(sys.argv[i+1]))
 				return 0
 		except:
-			print_info(1, False)
+			print_info(1)
 			return 1
-
 	handle_args_equations()
 
 
